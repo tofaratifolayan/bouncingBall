25e744d1fe1f62f0b57ceb19eec03fbfb02df36b
tofaratifolayan <82730510+tofaratifolayan@users.noreply.github.com>
2023-09-03 19:55:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let ball = document.getElementById('float-circle');
+
+// Write your code below
+const up = () => {
+  ball.style.bottom = '250px'
+}
+
+const down = () => {
+  ball.style.bottom = '50px'
+}
+
+document.addEventListener('keydown', up)
+
+document.addEventListener('keyup', down)
